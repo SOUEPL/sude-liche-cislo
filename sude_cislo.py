76190eac5eaa49c8dd9eb93b2d6706b90c72d614
petrleba <131344580+petrleba@users.noreply.github.com>
2023-04-27 22:00:29 +0200

    Create sude_cislo.py

---
+++ sude_cislo.py
@@ -0,0 +1,22 @@
+def check_entry(a):
+    try:
+        result = int(a)
+    except ValueError:
+        raise ValueError("Zadaná hodnota není číslo.")
+    return result
+
+
+def is_even(number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False
+
+
+vstup = "1234"
+cislo = check_entry(vstup)
+
+if is_even(cislo):
+    print("Zadané číslo je sudé.")
+else:
+    print("Zadané číslo je liché.")
